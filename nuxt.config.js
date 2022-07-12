9b555c98cda2a62e9bab71db9dcbf30d34d32f6d
Sideshow-Charles <charlesonuoha97@gmail.com>
2022-07-12 23:23:02 +0100

    see

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
-      // { rel: 'stylesheet', type: 'text/css', href: '/global.css' }
+      
     ]
   },
 
